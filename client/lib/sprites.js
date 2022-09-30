fbf1f6e37382a2aa91d34a07e3a4f9dd195e4d2c
unknown <dienruei@gmail.com>
2022-10-01 01:14:59 +0800

    remove tiny edge on top of pics

--- client/lib/sprites.js
+++ client/lib/sprites.js
@@ -1,40 +1,40 @@
 export default {
   // bird
-  birdCircuit: { h: 176, w: 181, x: 1740, y: 354 },
-  birdPaper: { h: 176, w: 182, x: 1376, y: 354 },
+  birdCircuit: { h: 176, w: 181, x: 1740, y: 355 },
+  birdPaper: { h: 176, w: 182, x: 1376, y: 355 },
 
   //birdHit
-  birdCircuitHit: { h: 176, w: 181, x: 1921, y: 354 },
-  birdPaperHit: { h: 176, w: 182, x: 1558, y: 354 },
+  birdCircuitHit: { h: 176, w: 181, x: 1921, y: 355 },
+  birdPaperHit: { h: 176, w: 182, x: 1558, y: 355 },
 
   // obstacle
-  obstacleGate: { h: 128, w: 168, x: 1036, y: 354 },
-  obstacleCode: { h: 128, w: 170, x: 1204, y: 354 },
+  obstacleGate: { h: 128, w: 168, x: 1036, y: 355 },
+  obstacleCode: { h: 128, w: 170, x: 1204, y: 355 },
 
   // obstacleHit
-  obstacleGateHit: { h: 128, w: 168, x: 1036, y: 482 },
-  obstacleCodeHit: { h: 128, w: 170, x: 1204, y: 482 },
+  obstacleGateHit: { h: 128, w: 168, x: 1036, y: 483 },
+  obstacleCodeHit: { h: 128, w: 170, x: 1204, y: 483 },
 
   // dino
-  dino: { h: 300, w: 206, x: 0, y: 354 },
-  dinoLeftLeg: { h: 300, w: 203, x: 206, y: 354 },
-  dinoRightLeg: { h: 300, w: 205, x: 409, y: 354 },
-  dinoDuckLeftLeg: { h: 240, w: 218, x: 614, y: 354 },
-  dinoDuckRightLeg: { h: 240, w: 204, x: 832, y: 354 },
+  dino: { h: 300, w: 206, x: 0, y: 355 },
+  dinoLeftLeg: { h: 300, w: 203, x: 206, y: 355 },
+  dinoRightLeg: { h: 300, w: 205, x: 409, y: 355 },
+  dinoDuckLeftLeg: { h: 240, w: 218, x: 614, y: 355 },
+  dinoDuckRightLeg: { h: 240, w: 204, x: 832, y: 355 },
   dinoDisappear: { h: 1, w: 1, x: 0, y: 0 },
 
   // other
-  cloud: { h: 120, w: 242, x: 3040, y: 354 },
+  cloud: { h: 120, w: 242, x: 3040, y: 355 },
   ground: { h: 354, w: 4000, x: 0, y: 0 },
 
   // Items
-  week: { h: 169, w: 180, x: 2102, y: 354 },
-  guitar: { h: 169, w: 169, x: 2273, y: 354 },
-  dance: { h: 169, w: 169, x: 2442, y: 354 },
-  band: { h: 169, w: 169, x: 2613, y: 354 },
-  eater: { h: 169, w: 181, x: 2782, y: 354 },
+  week: { h: 169, w: 180, x: 2102, y: 355 },
+  guitar: { h: 169, w: 169, x: 2273, y: 355 },
+  dance: { h: 169, w: 169, x: 2442, y: 355 },
+  band: { h: 169, w: 169, x: 2613, y: 355 },
+  eater: { h: 169, w: 181, x: 2782, y: 355 },
 
   // PowerUps
   bullet: { h: 94, w: 77, x: 2963, y: 355 },
-  food: { h: 108, w: 108, x: 3282, y: 354 },
+  food: { h: 108, w: 108, x: 3282, y: 355 },
 };
