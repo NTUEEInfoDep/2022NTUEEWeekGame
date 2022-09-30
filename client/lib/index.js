2c6aed3411e6a08412ae353e248efeb6c00e2496
unknown <dienruei@gmail.com>
2022-09-30 21:09:02 +0800

    add duck_button

--- client/lib/index.js
+++ client/lib/index.js
@@ -18,25 +18,35 @@ const game = new DinoGame(
 const isTouchDevice =
   'ontouchstart' in window ||
   navigator.maxTouchPoints > 0 ||
-  navigator.msMaxTouchPoints > 0
+  navigator.msMaxTouchPoints > 0 ||
+  "gesturestart" in window;
 
 const keycodes = {
   // up, spacebar
   JUMP: { 38: 1, 32: 1 },
   // down
   DUCK: { 40: 1 },
-}
+};
+const isradius = (x, y, r) => {
+  const center_x = game.circle.x,
+    center_y = game.circle.y,
+    center_r = game.circle.radius;
+  return (x - center_x) ** 2 + (y - center_y) ** 2 <= center_r ** 2;
+};
 
 const ontouchstart = ({ touches }) => {
-  if (touches.length === 1) {
-    game.onInput('jump')
-  } else if (touches.length === 2) {
-    game.onInput('duck')
+  console.log(touches[0].clientX);
+  console.log(window.innerHeight, window.innerWidth);
+  if (isradius(touches[0].clientX, touches[0].clientY)) {
+    // isradius(touches[0].clientX, touches[0].clientY)
+    game.onInput("duck");
+  } else if (touches.length === 1) {
+    game.onInput("jump");
   }
-}
-const ontouchend = ({ touches }) => {
-  game.onInput('stop-duck')
-}
+};
+const ontouchend = () => {
+  game.onInput("stop-duck");
+};
 const onKeyDown = ({ keyCode }) => {
   console.log('down')
   if (keycodes.JUMP[keyCode]) {
