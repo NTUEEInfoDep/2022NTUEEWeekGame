c9f98e6b96d13e69a803ef31c00a800f883574d3
rayray2002 <rayray2002.huang@gmail.com>
2022-10-05 17:51:38 +0800

    post reload at backup

--- backup/crawl.py
+++ backup/crawl.py
@@ -1,6 +1,9 @@
 import datetime
 import os
+import requests
 
 with open(f"./backup.log", "a+") as f:
     data = os.popen("curl -s https://weekgame.ntuee.org/api/leaderboard").read()
     f.write(str(datetime.datetime.now()) + data + "\n")
+
+requests.post("https://weekgame.ntuee.org/api/reloadLeaderboard")
